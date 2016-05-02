46c487e7229fc21b3148b6dcee7bb6de96061f35
jiangxuan <645762213@qq.com>
2016-05-02 21:06:44 +0800

    首先做成绩查询，现在对于成绩查询已经对目录进行了创建，并书写了基本的php内容

---
+++ src/chengjichaxun/yingyudengjichaxun.php
@@ -0,0 +1,19 @@
+<?php 
+	header('Content-Type:text/html;charset=utf-8');
+	date_default_timezone_set('Asia/Shanghai');
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>英语等级查询</title>
+ 	<link href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+ 	<script src="http://cdn.bootcss.com/jquery/2.2.1/jquery.min.js"></script>
+ 	<script src="http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+ 	<?php include '../tpl/nav.php' ?>
+ 	<h1>英语等级查询</h1>
+ </body>
+ </html>
---
+++ src/tpl/nav.php
@@ -0,0 +1,47 @@
+<div class="container-fluid">
+		<div class="row">
+			<nav class="navbar navbar-default">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#导航">
+							<span class="sr-only"></span>
+							<span class="iconbar"></span>
+							<span class="iconbar"></span>
+							<span class="iconbar"></span>
+						</button>
+						<a href="#" class="navbar-brand">学生端</a>
+					</div>
+					<div class="collapse navbar-collapse" id="导航">
+						<ul class="nav navbar-nav">
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">教务活动<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="jiaoxuepingjia.php">教学评价</a></li>
+									<li><a href="gongxuanxuanke.php">公选选课</a></li>
+									<li><a href="banjikebiao.php">班级课表</a></li>
+									<li><a href="kaoshiricheng.php">考试日程</a></li>
+								</ul>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">成绩查询<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="zhuanyekechengchaxun.php">专业课程查询</a></li>
+									<li><a href="yingyudengjichaxun.php">英语等级查询</a></li>
+									<li><a href="diannaodengjichaxun.php">电脑等级查询</a></li>
+								</ul>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">其他操作<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="yonghubangding.php">用户绑定</a></li>
+									<li><a href="jiaoshixinxi.php">教师信息</a></li>
+									<li><a href="xuexiaodaohang.php">学校导航</a></li>
+									<li><a href="shiyongshuoming.php">使用说明</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</div>
